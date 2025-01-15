3d1bbd0279340ea50fd0cba9de2732772b24f3d8
revand <vishnuscorpio215@gmail.com>
2025-01-15 10:32:11 +0700

    Add files via upload

---
+++ tugas.js
@@ -0,0 +1,17 @@
+
+let nama = "revand"; 
+let alamat = "semarang city"; 
+let usia = "16";  
+let ttl = "pekanbaru 15 november 2008"; 
+let gender = "laki laki"; 
+let agama = "islam"; 
+let email = "Vishnuscorpio215@gmail.com";
+let berat = "90";
+let tinggi = "180";
+let hobi = "bermain gim";
+let cita = "pengusaha di bidang IT";
+let pekerjaan = "siswa";
+let sekolah = "SMK N 1 SAYUNG";
+let jurusan = "PPLG";
+
+document.write("nama saya adalah " + nama + " saya tinggal di " + alamat + " pada tahun 2024 ini usia saya adalah " + usia + "tahun, saya lahir di " + ttl + "jenis kelamin saya adalah " + gender + "saya beragama " + agama + "alamat email saya adalah " + email + "saya memiliki berat badan " + berat +"kg"+ "dengan tinggi " + tinggi + "saya sangat senang " + hobi + "karena saya memiliki cita cita " + cita + "saya saat adalah seorang " + pekerjaan + "yang bersekolah di " + sekolah + "pada jurusan " + jurusan);
